a48bb8b7af9965d4cdae725ad6b3f7ab31cb4bbc
auzile <auzile.revan@gmail.com>
2018-08-11 16:39:22 +0700

    added newer version
    added finished contact form

---
+++ processor.php
@@ -0,0 +1,42 @@
+<?php
+	include_once('config.php');
+
+	if( isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['message'])){//if posted data for name,email & message are all set, then and only then the rest of the code below run
+		$n = $_POST['name']; // HINT: use preg_replace() to filter the data
+		$e = $_POST['email'];
+		$p = $_POST['phone'];
+		$m = nl2br($_POST['message']);//change newline to break tags
+
+		//sanitize all post vars
+		$name=mysqli_real_escape_string($con, $_POST['name']);
+		$nameclean=filter_var($name, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+		$email=mysqli_real_escape_string($con, $_POST['email']);
+		$emailclean=filter_var($email, FILTER_SANITIZE_EMAIL, FILTER_FLAG_STRIP_HIGH);
+		$phone=mysqli_real_escape_string($con, $_POST['phone']);
+		$phoneclean=filter_var($phone, FILTER_SANITIZE_NUMBER_INT, FILTER_FLAG_STRIP_HIGH); 
+		$message=mysqli_real_escape_string($con, $_POST['message']);
+		$messageclean=filter_var($message, FILTER_SANITIZE_EMAIL, FILTER_FLAG_STRIP_HIGH);
+
+		//insert into database
+		mysqli_query($con,"INSERT INTO contact(name,email,phone,message)VALUES('$nameclean','$emailclean','$phoneclean','$messageclean')");  
+
+		//send an email
+		$to = "auzile.green@gmail.com";//email address the massage to go to
+		$from = $e;
+		$subject = 'Contact Form Message';
+		$message = '<b>Name:</b>'.$n.'<br><b>Email:</b> '.$e.'<br><b>Phone:</b> '.$p.' <p>'.$m.'</p>';
+		$headers = "From: $from\n"; //specify who the email is form
+		$headers .= "MIME-Version: 1.0\n"; 
+		$headers .= "Content-type: text/html; charset=iso-8859-1\n";
+		if( mail($to, $subject, $message, $headers) ){//check if parameters on the mail function has run properly
+			echo "success";
+		} else {
+			echo "The server failed to send the message. Please try again later.";
+		}
+		
+	}
+
+	$con->close();
+
+
+?>
---
+++ submitFunction.js
@@ -0,0 +1,34 @@
+function _(id){
+	return document.getElementById(id);
+}
+
+function submitFunction(){
+	_("mybtn").disabled=true; //disable button
+	_("status").innerHTML = 'please wait ...'; //indication that data being process on the server
+	var formdata = new FormData();//append new key-value pairs into all of data that's gonna be transmitted to PHP
+	formdata.append("name", _("name").value);
+	formdata.append("email", _("email").value);
+	formdata.append("phone", _("phone").value);
+	formdata.append("message", _("message").value);
+	var ajax = new XMLHttpRequest();
+	ajax.open("POST", 'processor.php'); //Want to POST the data to the php script
+	ajax.onreadystatechange = function(){
+		if(ajax.readyState == 4 && ajax.status == 200){//The data is finishes processing by PHP and PHP has returned data to the ajax object
+			if(ajax.responseText == "success") {
+				var nameValue = _("name").value;
+				if(/\s/.test(nameValue)){
+					var substr = nameValue.substr(0,nameValue.indexOf(' '));
+					_("status").innerHTML = 'Thanks '+substr+' We will contact you soon';
+				}else{
+					_("status").innerHTML = 'Thanks '+nameValue+' We will contact you soon';	
+				}
+			} else {
+				_("status").innerHTML = ajax.responseText;
+				_("mybtn").disabled = false;
+			}
+
+			$("#contactform")[0].reset();
+		}
+	}
+	ajax.send( formdata );
+}
