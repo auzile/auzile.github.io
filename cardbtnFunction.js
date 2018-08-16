1e124dd58579c511cf2645e5e6a7959bb9a61e56
auzile <auzile.revan@gmail.com>
2018-08-16 09:22:58 +0700

    fixed the carousel button effects

--- cardbtnFunction.js
+++ cardbtnFunction.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
 
-	$(".divcontainer").hide();
+    $(".divcontainer").hide();
+    $(".projectdetail .jumbotron").hide();
 
 	$('.card a').click(function(e){
 		var target = "#" + $(this).data("target");
 		e.preventDefault();
-        $(".divcontainer").slideUp('fast');
-        $(target).slideDown('fast');	
+        $(".divcontainer").slideUp(function(){
+               $(target).show().siblings("div").hide();
+               $(".divcontainer").slideDown(); 
+        });
     });
 
     $('.exit').click(function(){
-    	$(".divcontainer").slideUp('fast');
+    	$(".divcontainer").slideUp();
     });
 
 });
